6f393e69a05c162f9ac897da4c39d01d86b78a1d
Erlend Fonnes <erlend@vimond.com>
2023-03-09 10:08:55 +0100

    fix: added option for failing on error to batch function

--- batch.go
+++ batch.go
@@ -3,10 +3,12 @@ package books
 import "math"
 
 // Batch can call the function f with batches of slice sl in sizes of batchSize.
-func Batch[D any, R any](req []D, batchSize int, f func([]D) ([]R, error)) ([]R, error) {
+// If failOnErr is true, it returns all responses and errors so far upon the first returned error from the function.
+func Batch[D any, R any](req []D, batchSize int, failOnErr bool, f func([]D) (R, error)) ([]R, []error) {
 	numBatches := len(req) / batchSize
 	ln := float64(len(req))
-	resp := make([]R, 0)
+	resp := make([]R, 0, numBatches)
+	errs := make([]error, 0, numBatches)
 
 	for i := 0; i <= numBatches; i++ {
 		currentBatch := req[i*batchSize : int(math.Min(ln, float64((i+1)*batchSize)))]
@@ -15,12 +17,13 @@ func Batch[D any, R any](req []D, batchSize int, f func([]D) ([]R, error)) ([]R,
 		}
 
 		r, err := f(currentBatch)
-		if err != nil {
-			return nil, err
-		}
+		resp = append(resp, r)
+		errs = append(errs, err)
 
-		resp = append(resp, r...)
+		if err != nil && failOnErr {
+			return resp, errs
+		}
 	}
 
-	return resp, nil
+	return resp, errs
 }
