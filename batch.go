8efef5da97319cd03993494d899a510be84d1337
Erlend Fonnes <erlend@vimond.com>
2023-03-14 09:34:19 +0100

    fix: slightly changed batch function, including inputs

--- batch.go
+++ batch.go
@@ -4,19 +4,13 @@ import "math"
 
 // Batch can call the function f with batches of slice sl in sizes of batchSize.
 // If failOnErr is true, it returns all responses and errors so far upon the first returned error from the function.
-func Batch[D any, R any](req []D, batchSize int, failOnErr bool, f func([]D) (R, error)) ([]R, []error) {
-	numBatches := len(req) / batchSize
-	ln := float64(len(req))
+func Batch[D any, R any](req []D, f func([]D) (R, error), batchSize int, failOnErr bool) ([]R, []error) {
+	numBatches := int(math.Ceil(float64(len(req)) / float64(batchSize)))
 	resp := make([]R, 0, numBatches)
 	errs := make([]error, 0, numBatches)
 
-	for i := 0; i <= numBatches; i++ {
-		currentBatch := req[i*batchSize : int(math.Min(ln, float64((i+1)*batchSize)))]
-		if len(currentBatch) == 0 {
-			continue
-		}
-
-		r, err := f(currentBatch)
+	for i := 0; i < len(req); i += batchSize {
+		r, err := f(req[i:Min(i+batchSize, len(req))])
 		resp = append(resp, r)
 		errs = append(errs, err)
 
